24a6e64f594d3bb33a23b02f379f40c63db30708
dtannen <dtannen@gmail.com>
2022-05-16 19:07:00 -0400

    add xkcd and bbc news

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -4,7 +4,7 @@ import { getProjects } from '../api';
 import { getQuote } from '../api';
 import { getReadme } from '../api';
 import { getWeather } from '../api';
-import { getStockPrice } from '../api';
+import Parser from "rss-parser";
 
 var yahooService = require('../../services/yahooService');
 var responseTransformer = require('../../transformer/responseTransformer');
@@ -48,4 +48,27 @@ export const stock = async (args: string[]): Promise<string> => {
   const tickers = stock.split(',').map((ticker) => ticker.trim());
   const quote_data = await yahooService.getCurrentPrice(tickers);
   return responseTransformer.transformCurrentPrice(quote_data);
+};
+
+export const news = async (args: string[]): Promise<string> => {
+  let parser = new Parser();
+  const PROXY_URL = window.location.protocol + '//' + window.location.hostname + ':8080/';
+  let feed = await parser.parseURL(PROXY_URL + "http://feeds.bbci.co.uk/news/world/rss.xml");
+  const news = "";
+  feed.items.forEach(item => {
+      news = news + `<a class="text-light-blue dark:text-dark-blue underline" href="${item.link}" target="_blank">${item.title}</a>\n`;
+  });
+  return `Current News...\n${news}`;
+};
+
+export const xkcd = async (args: string[]): Promise<string> => {
+  let parser = new Parser();
+  const PROXY_URL = window.location.protocol + '//' + window.location.hostname + ':8080/';
+  let feed = await parser.parseURL(PROXY_URL + "https://xkcd.com/rss.xml");
+  console.log(feed);
+  const news = "";
+  feed.items.forEach(item => {
+      news = news + `<a class="text-light-blue dark:text-dark-blue underline" href="${item.link}" target="_blank">${item.title}</a> - ${item.content}\n`;
+  });
+  return `${news}`;
 };
